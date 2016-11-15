67fc94ed07b9b86daee462b2aad3dd433ed6e714
Ryan Mathisen and Ryan Peterson <ryan+ryan@>
2016-11-15 11:28:55 -0800

    contain actions to one view

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -8,15 +8,15 @@ namespace Automobile
   {
     public HomeModule()
     {
-      Get["/"] = _ => View["add_car.cshtml"];
-      Get["/show_cars"] = _ =>{
+      Get["/"] = _ => {
         List<Car> allCars = Car.GetAll();
-        return View["show_cars.cshtml", allCars];
+        return View["add_car.cshtml", allCars];
       };
-      Post["/car_added"] = _ => {
+      Post["/"] = _ => {
       Car newCar = new Car(Request.Form["car-model"], Request.Form["car-price"], Request.Form["car-mileage"]);
       newCar.Save();
-      return View["car_added.cshtml", newCar];
+      List<Car> allCars = Car.GetAll();
+      return View["add_car.cshtml", allCars];
       };
     }
   }
